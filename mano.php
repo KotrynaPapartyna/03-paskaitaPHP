4033dfb1e19d015d305abb9700a721886333341d
KotrynaPapartyna <kmarkeviciute@gmail.com>
2021-07-30 10:03:06 +0300

    *

---
+++ mano.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>03 paskaitos-3/4uzduotis</title>
+</head>
+<body>
+
+<div class="prisijungimas">
+    <form action="index.php" method="get">
+        <input type="text" name="vardas" placeholder="Suveskite varda"/>
+        <input type="password" name="slaptazodis" placeholder="Suveskite slaptazodi"/>
+        <button type="submit" name="prisijungti"> Prisijungti</button>
+    </form>
+</div>
+   
+<?php
+
+
+?>
+
+
+</body>
+</html>
